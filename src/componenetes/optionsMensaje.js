e4b52acc309f5271808f8fdc82b6276585944c2a
facundo2309 <facundoomargarcia@gmail.com>
2022-05-19 22:42:00 -0300

    commit2

--- src/componenetes/optionsMensaje.js
+++ src/componenetes/optionsMensaje.js
@@ -4,12 +4,24 @@ export default  function OptionsMensaje (){
 
     const [selectedOptions, setSelected]= useState([])
 
-    const handelSelected
+    const handelSelected=(e)=>{
+
+        if(e.target.checked){
+
+            setSelected([...selectedOptions,e.target.value])
+        }
+
+        setSelected(selectedOptions.filter(o=> o !== e.target.value))
+
+
+    }
+
+    console.log(selectedOptions)
 
 
     return(
         <div>
-            <label><input id='invitacion' name='invitacion' type='checkbox' value='Invitacion'/>Invitacion</label>
+            <label><input onChange={(e)=>handelSelected(e)} id='invitacion' name='invitacion' type='checkbox' value='Invitacion'/>Invitacion</label>
             <label><input id='recordatorio'  name='recordatorio' type='checkbox' value='Recordatorio'/>Recordatorio de proceso</label>
             <label><input id='personalizado' name='personalizado' type='checkbox' value='Personalizado'/>Personalizado</label>
        </div>
